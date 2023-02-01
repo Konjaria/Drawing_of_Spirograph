02559b1ddf82237212dc616b862f7b9f6faa2317
Saba Konjaria <konjaria1010@gmail.com>
2023-02-01 04:19:07 +0400

    Update Drawing_Spirograph_Black_Color.py

--- Drawing_Spirograph_Black_Color.py
+++ Drawing_Spirograph_Black_Color.py
@@ -1,18 +1,18 @@
 import turtle
 import random
 
-Nu_Pagadzi = turtle.Turtle()
+painter = turtle.Turtle()
 
-Nu_Pagadzi.speed(0)
-current_heading = Nu_Pagadzi.heading()
-Nu_Pagadzi.hideturtle();
+painter.speed(0)
+current_heading = painter.heading()
+painter.hideturtle();
 
 
 def draw_spiragraph(size_of_gap):
     for i in range(int(360/size_of_gap)):
-        Nu_Pagadzi.pencolor("black")
-        Nu_Pagadzi.circle(100)
-        Nu_Pagadzi.setheading(Nu_Pagadzi.heading() + size_of_gap)
+        painter.pencolor("black")
+        painter.circle(100)
+        painter.setheading(painter.heading() + size_of_gap)
 
 
 draw_spiragraph(4)
